4f26d3b9cc2436136f6d063381d5c15fb2160048
E0SelmY4V-PC <2507917746@qq.com>
2023-01-21 01:31:03 +0800

    Add 各种全新设计

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
-import type { Proce } from 'scpo-proce';
+import type { Proce, ProceN } from 'scpo-proce';
 import scpoProce = require('scpo-proce');
+import * as child_process from 'child_process'
 
 /**
  * Lethal Build
@@ -15,12 +16,12 @@ class Opn {
 	}
 	dir = '';
 	t = (n: string, d = true) => d ? n ? (this.dir + '/' + n) : this.dir : n;
-	mergeOut = (list: string[], out: fs.WriteStream) =>
+	mergeOut = (list: string[], out: fs.WriteStream) => () =>
 		scpoProce.snake(list.map(e => (todo: () => void) =>
 			fs.createReadStream(e).on('end', todo).pipe(out, { end: false })
 		)).then(() => out.end());
 	tid = -1;
-	outFS = (k: [0 | 1 | boolean, string][], out: string | fs.WriteStream, d = true) => {
+	outFS = (k: [0 | 1 | boolean, string][], out: string | fs.WriteStream, d = true) => () => {
 		const files: string[] = [], temps: string[] = [];
 		const outs = typeof out === 'string' ? fs.createWriteStream(this.t(out, d)) : out;
 		return scpoProce
@@ -31,12 +32,18 @@ class Opn {
 					fs.writeFile(fname, e[1], todo);
 				} else files.push(this.t(e[1], d)), todo();
 			}))
-			.then(() => this.mergeOut(files, outs))
-			.take(() => this.dels(temps, false))
+			.then(this.mergeOut(files, outs))
+			.take(this.dels(temps, false), void 0, 1)
 			.take(1);
 	}
-	cps = (p: [string, string][], d = true): Proce<[]> =>
+	cps = (p: [string, string][], d = true) => (): Proce<[]> =>
 		scpoProce.snake(p.map(e => todo => (fs.cp(this.t(e[0], d), this.t(e[1], d), todo))));
-	dels = (p: string[], d = true): Proce<[]> =>
+	dels = (p: string[], d = true) => (): Proce<[]> =>
 		scpoProce.snake(p.map(e => todo => fs.unlink(this.t(e, d), todo)));
+	exec = (cmd: string) => (): Proce<[], [child_process.ExecException]> => scpoProce.snake(
+		todo => child_process.exec(cmd, todo),
+		(todo, ordo, err, stdout, stderr) => (console.log(stdout), console.log(stderr), err ? ordo : todo)(err),
+	);
+	judge = (...l: boolean[]) => () => scpoProce.snake(l.map(e => e ? _ => _() : _ => _));
+	snake = (...t: (() => ProceN)[]) => scpoProce.snake(t.map(e => todo => e().then(todo)));
 };
