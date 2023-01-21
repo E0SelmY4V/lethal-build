530c28c56cb88afe2e1045a77694d17482925a63
E0SelmY4V-PC <2507917746@qq.com>
2023-01-21 18:27:59 +0800

    Env 改协议，避免自己起诉自己

--- lib/index.ts
+++ lib/index.ts
@@ -3,54 +3,55 @@ import type { Proce, ProceN } from 'scpo-proce';
 import scpoProce = require('scpo-proce');
 import * as child_process from 'child_process'
 
-const initer = (dir: string) => new Opn(dir);
-initer.default = initer;
-
-class Opn {
-	constructor(dir: string) {
-		this.dir = dir;
-	}
-	dir = '';
-	t = (n: string, d = true) => d ? n ? (this.dir + '/' + n) : this.dir : n;
-	mergeOut = (list: string[], out: fs.WriteStream) => () =>
-		scpoProce.snake(list.map(e => (todo: () => void) =>
-			fs.createReadStream(e).on('end', todo).pipe(out, { end: false })
-		)).then(() => out.end());
-	tid = -1;
-	outFS = (k: [0 | 1 | boolean, string][], out: string | fs.WriteStream, d = true) => () => {
-		const files: string[] = [], temps: string[] = [];
-		const outs = typeof out === 'string' ? fs.createWriteStream(this.t(out, d)) : out;
-		return scpoProce
-			.snake(k.map(e => todo => {
-				if (e[0]) {
-					const fname = `${this.dir}/temp${++this.tid}`;
-					files.push(fname), temps.push(fname);
-					fs.writeFile(fname, e[1], todo);
-				} else files.push(this.t(e[1], d)), todo();
-			}))
-			.then(this.mergeOut(files, outs))
-			.take(this.dels(temps, false), void 0, 1)
-			.take(1);
-	}
-	cps = (p: [string, string][], d = true) => (): Proce<[]> =>
-		scpoProce.snake(p.map(e => todo => (fs.cp(this.t(e[0], d), this.t(e[1], d), todo))));
-	dels = (p: string[], d = true) => (): Proce<[]> =>
-		scpoProce.snake(p.map(e => todo => fs.unlink(this.t(e, d), todo)));
-	exec = (cmd: string) => (): Proce<[], [child_process.ExecException]> => scpoProce.snake(
-		todo => child_process.exec(cmd, todo),
-		(todo, ordo, err, stdout, stderr) => (console.log(stdout), console.log(stderr), err ? ordo : todo)(err),
-	);
-	judge = (...l: boolean[]) => () => scpoProce.snake(l.map(e => e ? _ => _() : _ => _));
-	snake = (...t: (() => PromiseLike<any>)[]) => scpoProce.snake(t.map(e => todo => e().then(todo)));
-	log = (...msg: any[]) => () => scpoProce((console.log(...msg), false));
-	initer = initer as typeof import('.');
-};
 /**
  * Lethal Build
  * @version 1.1.0
- * @license WTFPL
+ * @license GPL-3.0-or-later
  * @link https://github.com/E0SelmY4V/lethal-build
  */
-export = Object.assign(initer, {
-	Opn,
-});
+function initer(dir: string) {
+	return new initer.Opn(dir);
+}
+namespace initer {
+	export class Opn {
+		constructor(dir: string) {
+			this.dir = dir;
+		}
+		dir = '';
+		t = (n: string, d = true) => d ? n ? (this.dir + '/' + n) : this.dir : n;
+		mergeOut = (list: string[], out: fs.WriteStream) => () =>
+			scpoProce.snake(list.map(e => (todo: () => void) =>
+				fs.createReadStream(e).on('end', todo).pipe(out, { end: false })
+			)).then(() => out.end());
+		tid = -1;
+		outFS = (k: [0 | 1 | boolean, string][], out: string | fs.WriteStream, d = true) => () => {
+			const files: string[] = [], temps: string[] = [];
+			const outs = typeof out === 'string' ? fs.createWriteStream(this.t(out, d)) : out;
+			return scpoProce
+				.snake(k.map(e => todo => {
+					if (e[0]) {
+						const fname = `${this.dir}/temp${++this.tid}`;
+						files.push(fname), temps.push(fname);
+						fs.writeFile(fname, e[1], todo);
+					} else files.push(this.t(e[1], d)), todo();
+				}))
+				.then(this.mergeOut(files, outs))
+				.take(this.dels(temps, false), void 0, 1)
+				.take(1);
+		}
+		cps = (p: [string, string][], d = true) => (): Proce<[]> =>
+			scpoProce.snake(p.map(e => todo => (fs.cp(this.t(e[0], d), this.t(e[1], d), todo))));
+		dels = (p: string[], d = true) => (): Proce<[]> =>
+			scpoProce.snake(p.map(e => todo => fs.unlink(this.t(e, d), todo)));
+		exec = (cmd: string) => (): Proce<[], [child_process.ExecException]> => scpoProce.snake(
+			todo => child_process.exec(cmd, todo),
+			(todo, ordo, err, stdout, stderr) => (console.log(stdout), console.log(stderr), err ? ordo : todo)(err),
+		);
+		judge = (...l: boolean[]) => () => scpoProce.snake(l.map(e => e ? _ => _() : _ => _));
+		snake = (...t: (() => PromiseLike<any>)[]) => scpoProce.snake(t.map(e => todo => e().then(todo)));
+		log = (...msg: any[]) => () => scpoProce((console.log(...msg), false));
+		initer = initer as typeof import('.');
+	};
+}
+initer.default = initer;
+export = initer;
