f1fb43600d718f8864881bfcee8c4ca8b78d8f01
E0SelmY4V-PC <2507917746@qq.com>
2023-03-12 16:53:20 +0800

    Ref 完全异步改造

--- lib/index.ts
+++ lib/index.ts
@@ -25,6 +25,7 @@ function initer(dir: string) {
 	return initer.OpnList[dir] || (initer.OpnList[dir] = new initer.Opn(dir));
 }
 namespace initer {
+	export type Will<T> = T | PromiseLike<T>;
 	export const OpnList: { [dir: string]: Opn; } = {};
 	export const isRegExp = (n: RegExp | readonly any[]): n is RegExp => 'flags' in n;
 	export class Opn {
@@ -36,66 +37,78 @@ namespace initer {
 		comp = (fname: string, noIgn = true) => noIgn ? fname ? (this.dir + '/' + fname) : this.dir : fname;
 		t = this.comp;
 		private cmtStr = '';
-		setCmt = (cmdFile: string, { br = '\n', noIgn = true } = {}) => async () => {
+		private cmtNum = 0;
+		private cmtNow = 0;
+		private cmtEvent = new events.EventEmitter;
+		setCmt = (cmdFile: Will<string>, { br = '\n', noIgn = true } = {}) => async () => {
 			const cmtArr: string[] = [];
 			let cb = (input: string) => input[0] === ' ' || input[0] === '/' ? cmtArr.push(input) : inter.removeAllListeners('line');
-			const inter = readline.createInterface(fs.createReadStream(cmdFile)).on('line', cb);
+			const inter = readline.createInterface(fs.createReadStream(this.comp(await cmdFile, noIgn))).on('line', cb);
 			await events.once(inter, 'close');
 			this.cmtStr = cmtArr.join(br) + br;
+			this.cmtEvent.emit(`${this.cmtNum++}`);
 		};
-		cmt = () => this.cmtStr;
-		walk = async (dir = this.dir, matched: string[] = []) => {
-			const files = await fsp.readdir(dir);
+		getCmt = () => new Promise(res => this.cmtEvent.once(`${this.cmtNow++}`, res)).then(() => this.cmtStr);
+		walk = async (dir: Will<string> = this.dir, matched: Will<Will<string>[]> = []) => {
+			const files = await fsp.readdir(await dir);
 			await Promise.snake(files.map(filename => async (res) => {
-				const filepath = path.join(dir, filename);
-				(await fsp.stat(filepath)).isDirectory() ? await this.walk(filepath, matched) : matched.push(filepath);
+				const filepath = path.join(await dir, filename);
+				(await fsp.stat(filepath)).isDirectory() ? await this.walk(filepath, matched) : (await matched).push(filepath);
 				return res();
 			}));
 			return matched;
 		};
-		match = async (reg: RegExp | string[], dir = this.dir) => {
+		match = async (regWill: Will<RegExp | Will<string>[]>, dir: Will<string> = this.dir) => {
+			const reg = await regWill;
 			if (!isRegExp(reg)) return reg;
 			const allFile = await this.walk(dir);
 			const files: string[] = [];
-			allFile.forEach(file => reg.test(file) && files.push(file));
+			await Promise.snake(allFile.map(file => async res => (reg.test(await file) && files.push(await file), res())));
 			return files;
 		};
-		mergeOut = (files: string[] | RegExp, out: fs.WriteStream) => async () =>
-			Promise.snake((await this.match(files)).map(file => res =>
-				fs.createReadStream(file).on('end', res).pipe(out, { end: false })
+		mergeOut = (files: Will<Will<string>[] | RegExp>, out: fs.WriteStream) => async () =>
+			Promise.snake((await this.match(files)).map(file => async res =>
+				fs.createReadStream(await file).on('end', res).pipe(out, { end: false })
 			)).then(() => out.end());
 		tempFileId = -1;
-		outFS = (infos: [0 | 1 | boolean, string][], out: string | fs.WriteStream, noIgn = true) => () => {
+		outFS = (infosWill: Will<[0 | 1 | boolean, Will<string>][]>, outWill: Will<string | fs.WriteStream>, noIgn = true) => async () => {
+			const [infos, out] = [await infosWill, await outWill];
 			const files: string[] = [];
 			const temps: string[] = [];
 			const outs = typeof out === 'string' ? fs.createWriteStream(this.comp(out, noIgn)) : out;
-			return Promise
-				.snake(infos.map(([out, info]) => (res, rej) => {
-					if (out) {
-						const fname = `${this.dir}/temp${++this.tempFileId}`;
-						files.push(fname);
-						temps.push(fname);
-						fs.writeFile(fname, info, cbNoArg(res, rej));
-					} else {
-						files.push(this.comp(info, noIgn));
-						res();
-					}
-				}))
-				.then(this.mergeOut(files, outs))
-				.then(this.dels(temps, false));
+			await Promise.snake(infos.map(([out, info]) => async (res, rej) => {
+				if (out) {
+					const fname = `${this.dir}/temp${++this.tempFileId}`;
+					files.push(fname);
+					temps.push(fname);
+					fs.writeFile(fname, await info, cbNoArg(res, rej));
+				} else {
+					files.push(this.comp(await info, noIgn));
+					res();
+				}
+			}));
+			await this.mergeOut(files, outs)();
+			await this.dels(temps, false)();
 		};
-		cps = (opns: [string, string][], noIgn = true) => () =>
-			Promise.snake(opns.map(([from, to]) => (res, rej) => (fs.cp(this.comp(from, noIgn), this.comp(to, noIgn), cbNoArg(res, rej)))));
-		dels = (files: string[] | RegExp, noIgn = !isRegExp(files)) => async () =>
-			Promise.snake((await this.match(files)).map(file => (res, rej) => fs.unlink(this.comp(file, noIgn), cbNoArg(res, rej))));
-		exec = (cmd: string) => () => new Promise<void>((todo, ordo) =>
-			child_process.exec(cmd, cbArgs((out, err) => (console.log(out), console.log(err)), todo, ordo))
+		cps = (opns: Will<[Will<string>, Will<string>][]>, noIgn = true) => async () =>
+			Promise.snake((await opns).map(([from, to]) => async (res, rej) => (fs.cp(this.comp(await from, noIgn), this.comp(await to, noIgn), cbNoArg(res, rej)))));
+		dels = (files: Will<Will<string>[] | RegExp>, noIgn: boolean | null = null) => async () =>
+			Promise.snake((await this.match(files)).map(file => async (res, rej) => fs.unlink(this.comp(await file, noIgn ?? !isRegExp(await files)), cbNoArg(res, rej))));
+		exec = (cmd: Will<string>) => () => new Promise<void>(async (todo, ordo) =>
+			child_process.exec(await cmd, cbArgs((out, err) => (console.log(out), console.log(err)), todo, ordo))
 		);
-		judge = (...values: ((() => boolean) | boolean)[]) => () =>
-			Promise.snake(values.map(value => (typeof value === 'boolean' ? value : value()) ? giveAndDo : giveAndReturn));
-		snake = (...opns: (() => PromiseLike<any>)[]) =>
-			Promise.snake(opns.map(opn => todo => opn().then(todo)));
-		log = (...msg: any[]) => async () => (console.log(...msg), false);
+		judge = (...values: ((() => boolean) | Will<boolean>)[]) => () =>
+			Promise.snake(values.map(valueWill => async res => {
+				const value = await valueWill;
+				if (typeof (value) === 'boolean' ? value : value()) res();
+			}));
+		snake = (...opns: Will<() => PromiseLike<any>>[]) =>
+			Promise.snake(opns.map(opn => async todo => (await opn)().then(todo)));
+		log = <T>(...msgWill: T[]) => async () => {
+			const msgs: Awaited<T>[] = [];
+			await Promise.snake(msgWill.map(msg => async res => (msgs.push(await msg), res())));
+			console.log(...msgs);
+		};
 		initer = initer;
 	};
 }
